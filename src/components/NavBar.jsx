a0d1fb9d6534b3aa559cd69e22baa4e5c03a6f53
CourageToGroww <Monkeysbuymainstream@gmail.com>
2023-08-05 14:30:08 -0500

    added some better looking touches to navbar, route to home when clicking on stranger things, installed gsap animation library

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,20 +5,25 @@ import { FiMenu } from "react-icons/fi";
 const NavBar = () => {
   return (
     <>
-      <div className="sticky top-0 p-4 bg-gray-700 glass font-orbitron">
+      <div className="sticky top-0 p-4 bg-gray-700 text-rose-300 glass font-orbitron ">
         <div className="">
           <div className="flex justify-between">
             <nav className="flex space-x-4">
-              <Link className="text-red-400 hover:text-red-700" to="/posts">
+              <Link className="hover:text-red-700" to="/posts">
                 Posts
               </Link>
-              <Link className="text-red-400 hover:text-red-700" to="/profile">
+              <Link className="hover:text-red-700" to="/profile">
                 Profile
               </Link>
             </nav>
+            <div>
+              <Link className="hover:text-red-700" to="/">
+                Stranger Things
+              </Link>
+            </div>
             <div className="flex items-center space-x-4">
-              <FiMenu className="text-red-400 hover:text-red-700" />
-              <Link className="text-red-400 hover:text-red-700" to="/login">
+              <FiMenu className="hover:text-red-700" />
+              <Link className="hover:text-red-700" to="/login">
                 Login
               </Link>
             </div>
