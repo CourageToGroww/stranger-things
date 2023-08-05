0d65d5bb2e7415ebb61311415de1fb33608bb86d
CourageToGroww <Monkeysbuymainstream@gmail.com>
2023-08-05 01:06:03 -0500

    setting up project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./global.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1 className="text-center text-red-500 text-7xl "> Test</h1>
+    </>
+  );
 }
 
 export default App;
